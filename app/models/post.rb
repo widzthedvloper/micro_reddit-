5b0a32786be821de5c85dfae53566f31e7a01901
luisvinicius09 <luisvinicius0906@gmail.com>
2021-02-08 20:18:55 -0300

    Fix rubocop errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-    validates :title, presence: true, length: {minimum: 4, maximum: 25}
-    validates :body, presence: true, length: {minimum: 3, maximum: 256}
+    validates :title, presence: true, length: { minimum: 4, maximum: 25 }
+    validates :body, presence: true, length: { minimum: 3, maximum: 256 }
     belongs_to :user
     has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: {minimum: 3, maximum: 15}
-  validates :email, presence: true, length: {minimum: 7, maximum: 25}
-  validates :password, presence: true, length: {minumum: 6, maximum: 20}
+  validates :username, presence: true, length: { minimum: 3, maximum: 15 }
+  validates :email, presence: true, length: { minimum: 7, maximum: 25 }
+  validates :password, presence: true, length: { minumum: 6, maximum: 20 }
   validates :birthday, presence: true
   has_many :posts
   has_many :comments
