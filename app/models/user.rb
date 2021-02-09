97c535be4626d5e511be7469055ad54a6abdd0a9
luisvinicius09 <luisvinicius0906@gmail.com>
2021-02-09 11:16:52 -0300

    Remove custom field

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 3, maximum: 15 }
   validates :email, presence: true, length: { minimum: 7, maximum: 25 }
   validates :password, presence: true, length: { minumum: 6, maximum: 20 }
-  validates :birthday, presence: true
   has_many :posts
   has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2021_02_08_222632) do
     t.string "username"
     t.string "email"
     t.string "password"
-    t.date "birthday"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
